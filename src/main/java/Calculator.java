a80692a582e4f158d5800bb4d28c5ad416824f72
Paulina <paulinakozaczek@gmail.com>
2022-11-08 11:58:54 +0000

    new tests and functionality

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,10 +3,19 @@ public class Calculator {
         return firstNumber + secondNumber;
     }
 
+    public int subtract(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    public int multiply(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
     public double divide(int firstNumber, int secondNumber) {
         if(secondNumber == 0){
             throw new ArithmeticException("can't divide by zero");
         }
         return firstNumber / secondNumber;
     }
+
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -17,6 +17,18 @@ public class CalculatorTest {
     }
 
     @Test
+    void shouldReturn4When6SubtractedFrom10() {
+        int sum = calculator.subtract(10, 6);
+        Assertions.assertEquals(4, sum);
+    }
+
+    @Test
+    void shouldReturn20When10MultipliedBy2() {
+        int sum = calculator.multiply(10, 2);
+        Assertions.assertEquals(20, sum);
+    }
+
+    @Test
     void shouldReturn2When10DividedBy5() {
         double value = calculator.divide(10, 5);
         Assertions.assertEquals(2, value);
